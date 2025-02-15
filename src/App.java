2135a826f6a0fff2043a9c1cf596936fbb532f67
lucky634634 <khoa21102003@gmail.com>
2025-02-15 23:18:04 +0700

    Update

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class App {
@@ -6,9 +7,19 @@ public class App {
             System.err.println("Program requires at least one argument");
             System.exit(1);
         }
-        System.out.println(args[0]); 
+        System.out.println(args[0]);
+        ReadConfig("config.txt");
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter to continue");
         sc.nextLine();
         sc.close();
+    }
+
+    public static void ReadConfig(String path) {
+        File file = new File(path);
+        if (!file.exists())
+            return;
+        System.out.println(file.getName());
     }
 }
---
+++ src/Message.java
@@ -0,0 +1,5 @@
+public class Message {
+    public int fromId;
+    public String message;
+    public int toId;
+}
