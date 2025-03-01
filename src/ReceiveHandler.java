fb2cd17dd82647da1e68bab94cb77b44ed86f42d
2eggsnoodles <thmnhat21@clc.fitus.edu.vn>
2025-03-01 22:42:13 +0700

    modified:   src/ReceiveHandler.java

--- src/ReceiveHandler.java
+++ src/ReceiveHandler.java
@@ -36,29 +36,28 @@ public class ReceiveHandler extends Thread {
     }
 
     private void SES(Message message) {
-        System.out.println("Process");
-        // _process.PrintV_P();
-        // System.out.println("Message");
-        // for (int i = 0; i < message.v_p.length; i++) {
-        //     for (int j = 0; j < message.v_p.length; j++) {
-        //         System.out.print(message.v_p[i][j] + " ");
-        //     }
-        //     System.out.println(" ");
-        // }
         if (CheckDelivery(message)) {
             LogHandler.Log("Delivered: " + message.message + " from " + message.fromId, _process.logFile);
             _process.MergeV_P(message.v_p);
             DeliverFromBuffer();
             return;
         }
+        System.out.println("Process");
+        _process.PrintV_P();
+        System.out.println("Message");
+        for (int i = 0; i < message.v_p.length; i++) {
+            for (int j = 0; j < message.v_p.length; j++) {
+                System.out.print(message.v_p[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
         _bufferQueue.add(message);
         LogHandler.Log("Buffer: " + message.message + " from " + message.fromId, _process.logFile);
     }
 
     private boolean CheckDelivery(Message message) {
-        if (_process.v_p[message.fromId][message.toId] != message.v_p[message.fromId][message.toId] - 1) {
+        if (_process.v_p[message.fromId][message.toId] != message.v_p[message.fromId][message.toId] - 1)
             return false;
-        }
         for (int i = 0; i < message.v_p.length; i++) {
             if (i == message.fromId) {
                 continue;
